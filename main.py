f5f548290757cf5f7e2b4fc101324d27e683d319
kayla2000 <orimoloyekayode@outlook.com>
2024-05-04 21:22:46 -0400

    Update Blog name

--- main.py
+++ main.py
@@ -296,6 +296,6 @@ def contact():
 #         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
 #         connection.sendmail(MAIL_ADDRESS, MAIL_APP_PW, email_message)
 
-
 if __name__ == "__main__":
     app.run(debug=False, port=5001)
+
